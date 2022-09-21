79b737d0a8b5d718f0d2c32624a23dee574e9afd
Rocío Poza <rocio.poza.tejeda@gmail.com>
2022-09-21 22:57:19 +0200

    Add focus when the page is loaded, create todo when enter key is pressed and add input styles

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Todo from '@/types/Todo'
 
 Vue.use(Vuex)
 
+export const actionNames = {
+  POST_TODO: 'create_todo'
+}
+
+export const mutationNames = {
+  CREATE_TODO: 'create_todo'
+}
+
+export class StoreState {
+  todos: Array<Todo>
+
+  constructor() {
+    this.todos = []
+  }
+}
+
 export default new Vuex.Store({
-  state: {
+  state: new StoreState(),
+  actions: {
+    [actionNames.POST_TODO]: async ({commit}, todo: string) => {
+      commit(mutationNames.CREATE_TODO, todo)
+    }
   },
   mutations: {
+    [mutationNames.CREATE_TODO]: (state: StoreState, todo: string) => {
+      state.todos.push({
+        label: todo,
+        isComplete: false
+      })
+    }
   },
-  actions: {
+  getters: {
   },
   modules: {
   }
